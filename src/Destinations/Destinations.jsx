a54d00f7ad2078bc2ab321ec345fbe5027930cbf
Zack <168221270+Zval321@users.noreply.github.com>
2025-04-01 20:57:52 -0500

    added logic for many images

--- src/Destinations/Destinations.jsx
+++ src/Destinations/Destinations.jsx
@@ -30,9 +30,17 @@ export function Destinations() {
           </h1>
           <h1>{d.city}</h1>
           <p>{d.description}</p>
+          {d.images && d.images.length > 0 ? (
+            <div>
+              {d.images.map((image) => (
+                <img key={image.id} src={image.image_url} alt={`image of ${d.city}`} />
+              ))}
+            </div>
+          ) : (
+            <p>No Images available</p>
+          )}
         </div>
       ))}
-
     </div>
   )
 }
