61d0468039874025b3da3c0f4a807bfde85fe43b
Zack <168221270+Zval321@users.noreply.github.com>
2025-04-14 21:37:56 -0500

    attempting to fix the create page to include images

--- src/CreateNewDestination/CreateNewDestination.jsx
+++ src/CreateNewDestination/CreateNewDestination.jsx
@@ -1,16 +1,36 @@
 import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate} from 'react-router-dom'
 import './CreateNewDestination.css'
+import { useState } from 'react'
 
 export function CreateNewDestination() { 
   const navigate = useNavigate()
+  const [imageUrls, setImageUrls] = useState(['']);
+
+  const handleImageUrlChange = (index, value) => {
+    const newImageUrls = [...imageUrls];
+    newImageUrls[index] = value;
+    setImageUrls(newImageUrls);
+  };
+
+  const addImageUrlField = () => {
+    setImageUrls([...imageUrls, '']);
+  };
   
   const handleCreate = async (event) => { 
     event.preventDefault()
     const params = new FormData(event.target)
     try { 
       const response = await axios.post('http://localhost:3000/destinations.json', params)
+      const destinationID = response.data.id
       console.log(response.data)
+
+      for (const url of imageUrls) {
+        await axios.post('http://localhost:3000/images.json', {
+          destination_id: destinationId,
+          image_url: url,
+        });
+      }
     } catch(error) { 
       console.log(error)
     }
@@ -19,42 +39,43 @@ export function CreateNewDestination() {
   }
 
 
-  return ( 
-  <div className='form-wrapper'>
-    <h1>New Destination</h1>
-    <form 
-      onSubmit={handleCreate}
-      className='form-create'
-    >
-      <label>City:
-        <input 
-          className='city-input'
-          type='text'
-          placeholder='city'
-          name='city'  
-          required>
-        </input>
-      </label>
-      <label>Country:
-        <input 
-          className='country-input'
-          type='text'
-          placeholder='country'
-          name='country' 
-          required>
-        </input>
-      </label>
-      <label>Description:
-        <textarea 
-          className='description-input'
-          type='text'
-          placeholder='Description'
-          name='description' 
-          required>
-        </textarea>
-      </label>
-      <button type='submit'> Create</button>
-    </form>
-  </div>
-  )
+  return (
+    <div className="form-wrapper">
+      <h1>New Destination</h1>
+      <form onSubmit={handleCreate} className="form-create">
+        <label>
+          City:
+          <input className="city-input" type="text" placeholder="city" name="city" required />
+        </label>
+        <label>
+          Country:
+          <input className="country-input" type="text" placeholder="country" name="country" required />
+        </label>
+        <label>
+          Description:
+          <textarea
+            className="description-input"
+            placeholder="Description"
+            name="description"
+            required
+          ></textarea>
+        </label>
+        {imageUrls.map((url, index) => (
+          <label key={index}>
+            Image URL {index + 1}:
+            <input
+              type="text"
+              value={url}
+              onChange={(e) => handleImageUrlChange(index, e.target.value)}
+              required
+            />
+          </label>
+        ))}
+        <button type="button" onClick={addImageUrlField}>
+          Add Another Image
+        </button>
+        <button type="submit">Create</button>
+      </form>
+    </div>
+  );
 }
