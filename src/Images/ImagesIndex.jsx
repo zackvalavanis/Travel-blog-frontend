8b23df146fd79299c13527c99c4941f184d7e65d
Zack <168221270+Zval321@users.noreply.github.com>
2025-04-19 19:40:01 -0500

    added delete function to images

--- src/Images/ImagesIndex.jsx
+++ src/Images/ImagesIndex.jsx
@@ -1,26 +1,68 @@
 import { useLocation } from 'react-router-dom';
+import { useState } from 'react';
+import './ImagesIndex.css';
+import axios from 'axios'
 
+export function ImagesIndex() {
+  const location = useLocation();
+  const images = location.state?.images || [];
 
+  const [fullscreenImage, setFullscreenImage] = useState(null);
 
-export function ImagesIndex({id}) { 
-  const location = useLocation()
-  const images = location.state?.images || []
+  const handleImageClick = (imgUrl) => {
+    setFullscreenImage(imgUrl);
+  };
 
-  return ( 
+  const handleCloseModal = () => {
+    setFullscreenImage(null);
+  };
+
+  const handleDeleteImage = async (imageId) => {
+    try { 
+      await axios.delete(`http://localhost:3000/images/${imageId}.json`)
+      const updatedImages = images.filter((img) => img.id !== imageId)
+      location.state.imafes = updatedImages
+      window.location.reload()
+    } catch (err) { 
+      console.error('Failed to delete image', err)
+    }
+  }
+
+  return (
     <div>
-      <h1>All Images</h1>
+      <h1
+        className='header-images'
+      >
+        All Images
+      </h1>
+      <div className="image-grid">
       {images.length > 0 ? (
         images.map((img, index) => (
-          <img 
-            key={index}
-            src={img.image_url}
-            alt={`Destination Image ${index + 1}`}
-            style={{ width: "300px", padding: "10px" }}
-          />
+          <div className="image-card" key={index}>
+            <img
+              src={img.image_url}
+              alt={`Destination Image ${index + 1}`}
+              className="thumbnail"
+              onClick={() => handleImageClick(img.image_url)}
+            />
+            <button 
+              className='delete-button'
+              onClick={() => handleDeleteImage(img.id)} // <== wrap it in a function
+            >
+              Delete Image
+            </button>
+          </div>
         ))
-      ) : (
+        ) : (
         <p>No images available.</p>
+        )}
+      {/* Fullscreen Modal */}
+      {fullscreenImage && (
+        <div className="modal-overlay" onClick={handleCloseModal}>
+          <img className="modal-image" src={fullscreenImage} alt="Full view" />
+        </div>
       )}
+    </div>
     </div>
   );
 }
